a4751d113aba91b17f7887b36c707223ba2c1708
Vishesh <visheshruparelia18@gmail.com>
2017-10-22 19:27:11 +0530

    Mailer working,added ENV variables,SMTP set up

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
-
+local_env = File.join(Rails.root, 'config', 'local_env.rb')
+load(local_env) if File.exists?(local_env)
 # Initialize the Rails application.
 Rails.application.initialize!

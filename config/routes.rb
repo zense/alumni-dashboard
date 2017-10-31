fc5bd6c190f2b222ffafc91e3b46dccee8d66e84
Vishesh <visheshruparelia18@gmail.com>
2017-10-31 23:06:58 +0530

    Added sign out

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :alumni
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "alumni#index"
 end

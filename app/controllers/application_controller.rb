6e752d0d0ffedbed41aa78c2bbab16f2d83ef047
Vishesh <visheshruparelia18@gmail.com>
2017-10-31 23:56:22 +0530

    Added devise for dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def after_sign_in_path_for(resource)
+    if current_alumnus.admin == true
+      rails_admin.dashboard_path
+    else
+      root_path
+    end
+  end
 end

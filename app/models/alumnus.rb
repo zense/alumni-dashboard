39d46284f0ef65252ecaf8502c2039429a062448
administrator <administrator@Docker.iiitb.ac.in>
2018-02-08 21:13:07 +0530

    skip email validations

--- app/models/alumnus.rb
+++ app/models/alumnus.rb
@@ -1,24 +1,29 @@
 class Alumnus < ApplicationRecord
   has_many :regs, dependent: :destroy
   has_many :events, :through=> :regs
+
+  # Non-persistent attribute to allow creating a new user without a password
+  # Password will be set by the user by following a link in the invitation email
+  attr_accessor :allow_blank_password
+  attr_accessor :allow_blank_email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Non-persistent attribute to allow creating a new user without a password
-  # Password will be set by the user by following a link in the invitation email
-  attr_accessor :allow_blank_password
-
-  devise :validatable
-
   # Called by Devise to enable/disable password presence validation
   def password_required?
     allow_blank_password ? false : super
   end
+  def email_required?
+    allow_blank_email ? false : super
+  end
+
 
   # Don't require a password when importing users
   def before_import_save(record)
     self.allow_blank_password = true
+    self.allow_blank_email = true
   end
 end
